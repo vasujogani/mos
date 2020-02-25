61a3c963db10e9c1b2751f659ae4c8f8241c450f
Arjun Kunjilwar <arjunkun@cs.utexas.edu>
2020-02-25 00:05:01 -0600

    Added initialization for vspace

--- include/spawn/spawn.h
+++ include/spawn/spawn.h
@@ -22,7 +22,11 @@ struct spawninfo {
 
     // Information about the binary
     char * binary_name;     // Name of the binary
-
+    struct capref *l1_cnode; // pointer to process L1 CNODE
+    struct capref *l1_pagetable;
+    struct paging_state ps;
+    struct cnoderef l2_cnodes[ROOTCN_FREE_SLOTS]; // array
+    genvaddr_t entry_addr; 
     // TODO: Use this structure to keep track
     // of information you need for building/starting
     // your new process!
--- lib/spawn/spawn.c
+++ lib/spawn/spawn.c
@@ -24,11 +24,13 @@ errval_t spawn_load_by_name(void * binary_name, struct spawninfo * si) {
     // - Get the binary from multiboot image
     struct mem_region *mem_region = multiboot_find_module(bi, (const char *)binary_name);
 
-    // don't know if we need this yet
     struct capref child_frame = {
         .cnode = cnode_module,
-        .slot = mem_regioin−>mrmod_slot,
+        .slot = mem_region−>mrmod_slot,
     };
+
+    // map child_frame into new process virtual address space
+    // vaddr will be return address, vsize return size
 
     // wiill be used for verification later
     char *binary_start = (char *)mem_region->mr_base;
@@ -45,40 +47,22 @@ errval_t spawn_load_by_name(void * binary_name, struct spawninfo * si) {
         debug_printf("Unable to create initial L1 CNode\n");
         return err_push(err, LIB_ERR_CNODE_CREATE);
     }
+    si->l1_cnode = &l1_cnode;
 
-    // create TASKCN L2 Cnode and add appropriate capabilities
-    struct cnoderef taskcn_ref;
-    err = cnode_create_foreign_l2(l1_cnode, ROOTCN_SLOT_TASKCN, taskcn_ref);
-    if (err_is_fail(err)) {
-        debug_printf("Unable to create initial L1 CNode\n");
-        return err_push(err, LIB_ERR_CNODE_CREATE);
-    }
-    struct capref dispatcher_ref = {
-        .cnode = taskcn_ref,
-        .slot = TASKCN_SLOT_DISPATCHER,
-    };
-    // capability for dispatcher
-    err = dispatcher_create(dispatcher_ref);
-    if (err_is_fail(err)) {
-        debug_printf("Unable to create dispatcher capref\n");
-        return err;
-    }
-    // capability for endpoint
-    struct capref endpoint_ref = {
-        .cnode = taskcn_ref,
-        .slot = TASKCN_SLOT_SELFEP,
-    };
-    err = cap_retype(endpoint_ref, dispatcher_ref, 0,
-                    ObjType_EndPoint, OBJSIZE_DISPATCHER, 1);
-    if (err_is_fail(err)) {
-        return err_push(err, LIB_ERR_CNODE_CREATE_FROM_MEM);
-    }
+    for (int i = 0; i < ROOTCN_FREE_SLOT; i++) {
+        struct cnoderef cnode_ref;
+        err = cnode_create_foreign_l2(l1_cnode, i, cnode_ref);
+        if (err_is_fail(err)) {
+            debug_printf("Unable to create L2 CNode\n");
+            return err_push(err, LIB_ERR_CNODE_CREATE);
+        }
+        si->l2_cnodes[i] = cnode_ref;
+    } 
+
+    
     // copy L1 root capability
-    struct capref root_ref = {
-        .cnode = taskcn_ref,
-        .slot = TASKCN_SLOT_ROOTCN,
-    };
-    err = cap_copy(root_ref, l1_cnode);
+    curr_ref.slot = TASKCN_SLOT_ROOTCN;
+    err = cap_copy(curr_ref, l1_cnode);
     if (err_is_fail(err)) {
         return err_push(err, LIB_ERR_CNODE_CREATE_FROM_MEM);
     }
@@ -96,18 +80,35 @@ errval_t spawn_load_by_name(void * binary_name, struct spawninfo * si) {
     //allocate memory for process
     struct cnoderef base_page_cn_ref;
     err = cnode_create_foreign_l2(l1_cnode, ROOTCN_SLOT_BASE_PAGE_CN, base_page_cn_ref);
-    for (int i=0; i<L2_CNODE_SLOTS; i++) {
-        struct capref ram_ref = {
+    struct capref ram_ref = {
             .cnode = base_page_cn_ref,
-            .slot = i,
-        };
-        err = mm_alloc(&aos_mm, BASE_PAGE_SIZE, &ram_ref);
+            .slot = 0,
+    };
+    for (int i=0; i<L2_CNODE_SLOTS; i++) {
+        struct capref temp_cap;
+        err = ram_alloc_aligned(&temp_cap, BASE_PAGE_SIZE, BASE_PAGE_SIZE);
         if (err_is_fail(err)) {
             return err_push(err, LIB_ERR_CNODE_CREATE_FROM_MEM);
         }
+        err = cap_copy(ram_ref, temp_cap);
+        if (err_is_fail(err)) {
+            return err_push(err, LIB_ERR_CNODE_CREATE_FROM_MEM);
+        }
+        ram_ref.slot = i;
+        cap_destroy(temp_cap);
     }
     
-    err = cnode_create_foreign_l2(l1_cnode, ROOTCN_SLOT_PAGECN, NULL);
+    struct cnoderef page_table_cn_ref;
+    err = cnode_create_foreign_l2(l1_cnode, ROOTCN_SLOT_PAGECN, page_table_cn_ref);
+    struct capref l1_ref = {
+        .cnode = page_table_cn_ref,
+        .slot = 0,
+    };
+    err = vnode_create(l1_ref, s ObjType_VNode_ARM_l1);
+    err = paging_init_state(&ps, 0, l1_ref, get_default_slot_allocator());
+
+    err = elf_load(EM_ARM, elf_alloc_func, (void *)si, vaddr, vsize, &(si->entry_addr));
+
 
 
     // TODO: Implement me
@@ -119,4 +120,37 @@ errval_t spawn_load_by_name(void * binary_name, struct spawninfo * si) {
     // - Make dispatcher runnable
 
     return SYS_ERR_OK;
+}
+
+errval_t spawn_setup_dispatcher(struct spawninfo * si) {
+    struct capref curr_ref = {
+        .cnode = si->l2_cnodes[ROOTCN_SLOT_TASKCN],
+        .slot = TASKCN_SLOT_DISPATCHER,
+    };
+    // capability for dispatcher
+    err = dispatcher_create(curr_ref);
+    if (err_is_fail(err)) {
+        debug_printf("Unable to create dispatcher capref\n");
+        return err;
+    }
+    // capability for endpoint
+    struct capref dispatcher_ref = curr_ref;
+    curr_ref.slot = TASKCN_SLOT_SELFEP;
+    err = cap_retype(curr_ref, dispatcher_ref, 0,
+                    ObjType_EndPoint, OBJSIZE_DISPATCHER, 1);
+    if (err_is_fail(err)) {
+        return err_push(err, LIB_ERR_CNODE_CREATE_FROM_MEM);
+    }
+
+}
+
+errval_t elf_alloc_func(void *state, genvaddr_t base, size_t size, uint32_t flags, void **ret) {
+    base -= base % BASE_PAGE_SIZE;
+    size += size % BASE_PAGE_SIZE;
+    struct capref frame_ref;
+    size_t returned_bytes;
+    frame_alloc(&frame_ref, size, &returned_bytes);
+    paging_map_fixed_attr(state->ps, base, frame_ref, size, flags);
+    paging_map_fixed_attr(get_current_paging_state(), base, frame_ref,f size, flags);
+    *ret = (void *)base;
 }
